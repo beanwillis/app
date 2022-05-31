73092ef1f2abf4d05caa6b2fc7f567f0ce2169e6
ptvvo.2018 <ptvvo.2018@scis.smu.edu.sg>
2022-05-31 18:47:56 +0700

    update contact section, fixed css issue on intro section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,32 @@
 import './contact.scss'
 import particlesSkyConfig from "../../config/particlesSkyConfig"
 import Particles from 'react-particles-js';
+import { LinkedIn, Mail } from "@material-ui/icons"
 
 export default function Contact() {
     return (
         <div className='contact' id='contact'>
 
-            <Particles params={particlesSkyConfig} className="canvas"></Particles> 
+            <Particles params={particlesSkyConfig} className="canvas"></Particles>
             <div className="wrapper">
                 <div className="content">
-                    <h1>Contact Form</h1>
+                    <div className="title">
+
+                    <h2>Contacts</h2>
+                    </div>
+                    <div className="contact-method">
+                        <LinkedIn className="icon" />
+                        <h4>/vivo1310</h4>
+
+                    </div>
+                    <div className="contact-method">
+
+                        <Mail className="icon" />
+                        <h4>vptv1310@gmail.com</h4>
+                    </div>
+
+
+
                 </div>
             </div>
 
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,14 +11,11 @@ export default function Intro() {
     <div className="intro" id="intro">
         <Particles params={particlesBubbleConfig} className="canvas"></Particles> 
         <div className="imgContainer">
-                <img src={vi} alt="Vo Pham Thao Vi's profile picture. Short curly hair girl with a shining smile!" />
+                <img src={vi} alt="Vo Pham Thao Vi's profile. Short curly hair girl with a shining smile!" />
             </div>
         <div className="wrapper">
-
-
             <div className="content">
                 <h2>Hi! I'm Vi</h2>
-                
                 <Typical
                     steps={[
                         "SMU Undergraduate", 1000, 
@@ -34,28 +31,7 @@ export default function Intro() {
         <a href="#portfolio">
             <ExpandMore className="arrow" />
         </a>
-        {/* <div className="left">
-            <div className="imgContainer">
-            <img src={vi} alt="" />
-            </div>
-        </div>
-        <div className="right">
-            <div className="wrapper">
-            <h2>Hi! I'm Vi</h2> */}
 
-            {/* <Typical
-                steps={["SMU Undergraduate ", 1000, "Front-end Developer", 1000]}
-                loop={Infinity}
-                wrapper="h3"
-            /> */}
-            
-{/* 
-            <a href="#portfolio">
-                <ExpandMore className="arrow" />
-            </a>
-            </div>
-        </div> */}
-        
     </div>
   );
 }
